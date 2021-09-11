d09c62589e769d3a605f5bcf45df6880250d4ea7
Dolglas Mesquita <dolglas_mesquita@hotmail.com>
2021-09-11 17:42:19 -0300

    Versao da api com dados hardcode

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request
+from resources.recipes import recipes
+import random
+
+
+app = Flask(__name__)
+
+def findRecipe(id):  # HELPER BUSCA
+    for recipe in recipes:
+        if recipe['id'] == id:
+            return recipe
+
+@app.route('/recipes', methods=['GET']) # GET ALL
+def getAll():
+    return { 'recipes' : recipes }
+
+
+@app.route('/recipes/<int:id>', methods=['GET']) #GET POR ID
+def getById(id):
+    recipe = findRecipe(id)
+    if recipe:
+        return recipe
+    return {'message': 'Not found!'}, 404
+
+
+@app.route('/recipes', methods=['POST']) # POST
+def postRecipe():
+    data = request.get_json()
+    new_recipes = {
+        'id':  random.randrange(1, 9999),
+        'name': data['name'],
+        'ingredients': data['ingredients'],
+        'description': data['description']
+    }
+    recipes.append(new_recipes)
+    return 'Cadastrado com sucesso', 201
+
+
+@app.route('/recipes/<int:id>', methods=['PUT']) # PUT
+def updateRecipe(id):
+    data = request.get_json()
+    recipe = findRecipe(id)
+    new_recipes = {
+        'id':  id,
+        'name': data['name'],
+        'ingredients': data['ingredients'],
+        'description': data['description']
+    }
+    if recipe:
+        recipe.update(new_recipes)
+        return 'atualizado com sucesso!', 200
+    return 'Not found!', 404
+
+
+@app.route('/recipes/<int:id>', methods=['DELETE']) # DELETE
+def gdeleteRecipe(id):
+    recipe = findRecipe(id)
+    if recipe:
+        global recipes
+        recipes = [recipe for recipe in recipes if recipe['id'] != id]
+        return 'Deletado com sucesso!', 200    
+    return {'message': 'Not found!'}, 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ resources/recipes.py
@@ -0,0 +1,62 @@
+recipes = [
+    {
+        'id': 1,
+        'name': 'Macarronada',
+        'ingredients': 'macarrão',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+        'id': 2,
+        'name': 'Omelete',
+        'ingredients': 'ovos',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+         'id': 3,
+        'name': 'Bolo',
+        'ingredients': 'ovos, leite, trigo, fermento',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+        'id': 4,
+        'name': 'Lasanha',
+        'ingredients': 'massa para lasanha, queijo, carne',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+        'id': 5,
+        'name': 'Salada',
+        'ingredients': 'alface, tomate, cebola',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+        'id': 6,
+        'name': 'Cokies',
+        'ingredients': 'chocolate, trigo',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+        'id': 7,
+        'name': 'Pudim',
+        'ingredients': 'ovos, leite, leite condensado, açúcar',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+        'id': 8,
+        'name': 'Sorvete',
+        'ingredients': 'gelo, água, suco',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+        'id': 9,
+        'name': 'Pizza',
+        'ingredients': 'trigo, queijo, ovos, cebola, tomate',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    },
+    {
+        'id': 10,
+        'name': 'Parmegiana',
+        'ingredients': 'frango, queijo',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus, lorem nec porta viverra, nibh ante maximus ex, vitae imperdiet mauris est in ipsum.'
+    }
+]
